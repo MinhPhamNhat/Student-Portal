be669dbbdd993a5c1441447a6b2a049dae1e38fd
Than-Trong-Huynh-Nhan <73160254+MinhPhamNhat@users.noreply.github.com>
2021-05-12 14:53:40 +0700

    a

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use(function(err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render('error', {user: req.user});
 });
 
 server.listen(PORT, () => console.log(`Listen on: http://localhost:${PORT}`))
